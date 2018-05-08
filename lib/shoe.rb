f76df04d10e06a78ae1262d4ce094ae387bf3f79
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-08 22:25:26 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,5 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material
-  attr_reader :condition
+  attr_accessor :brand, :color, :size, :material, :condition
   def initialize(brand)
     @brand = brand
   end
